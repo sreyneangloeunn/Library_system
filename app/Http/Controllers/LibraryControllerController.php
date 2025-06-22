d86acb9acefbade44387568b44ab66cab4e19e7d
sreyneangloeunn <sreyneang.loeun@student.passerellesnumeriques.org>
2025-06-22 14:17:44 +0700

    update DB

---
+++ app/Http/Controllers/LibraryControllerController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LibraryController;
+use Illuminate\Http\Request;
+
+class LibraryControllerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LibraryController $libraryController)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, LibraryController $libraryController)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(LibraryController $libraryController)
+    {
+        //
+    }
+}
