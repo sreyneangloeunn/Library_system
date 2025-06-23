e4c027ff7373588cbb01cce835a106c91b54a662
sreyneangloeunn <sreyneang.loeun@student.passerellesnumeriques.org>
2025-06-23 15:52:41 +0700

    connect with vue js

--- routes/api.php
+++ routes/api.php
@@ -21,9 +21,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 
-Route::get('library', [LibraryController::class, 'index']);
-Route::post('library', [LibraryController::class, 'store']);
-Route::put('library/{id}', [LibraryController::class, 'update']);
-Route::patch('library/{id}', [LibraryController::class, 'update']);
-Route::delete('library/{id}', [LibraryController::class, 'destroy']);
+Route::get('libraries', [LibraryController::class, 'index']);
+Route::post('libraries', [LibraryController::class, 'store']);
+Route::put('libraries/{id}', [LibraryController::class, 'update']);
+Route::patch('libraries/{id}', [LibraryController::class, 'update']);
+Route::delete('libraries/{id}', [LibraryController::class, 'destroy']);
 
